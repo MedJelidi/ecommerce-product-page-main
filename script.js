80c9a81f38f712aa94dea2d24c05db6024556422
MedJelidi <mohammedjelidi05@gmail.com>
2022-01-16 11:42:02 +0100

    quantity

--- script.js
+++ script.js
@@ -3,19 +3,26 @@ const menuClose = document.querySelector('.icon-close')
 const menu = document.querySelector('.menu')
 const blackLayer = document.querySelector('.black-layer')
 const cartIcon = document.querySelector('.cart-icon')
+const cartIconImg = cartIcon.querySelector('img')
 const cartContainer = document.querySelector('.cart-container')
 const images = document.querySelector('.images')
 const imagesNumber = images.querySelectorAll('img').length
 const prevArrow = document.querySelector('.prev-arrow')
 const nextArrow = document.querySelector('.next-arrow')
+const minusIcon = document.querySelector('.icon-minus')
+const plusIcon = document.querySelector('.icon-plus')
+const quantityElem = document.querySelector('.quantity')
 
 let activeIndex = 0
+let quantity = 0
 
 menuOpen.addEventListener('click', openMenu)
 menuClose.addEventListener('click', closeMenu)
 cartIcon.addEventListener('click', toggleCart)
 nextArrow.addEventListener('click', () => slide('next'))
 prevArrow.addEventListener('click', () => slide('prev'))
+minusIcon.addEventListener('click', removeQuantity)
+plusIcon.addEventListener('click', addQuantity)
 
 function openMenu() {
     menu.classList.add('show-menu')
@@ -29,6 +36,7 @@ function closeMenu() {
 
 function toggleCart() {
     cartContainer.classList.toggle('show-cart')
+    cartIconImg.classList.toggle('black-cart')
 }
 
 function slide(direction) {
@@ -38,4 +46,12 @@ function slide(direction) {
         activeIndex = activeIndex <= 0 ? imagesNumber - 1 : activeIndex - 1
     }
     images.style.transform = `translateX(-${100 * activeIndex}%)`
+}
+
+function addQuantity() {
+    quantityElem.innerText = `${++quantity}`
+}
+
+function removeQuantity() {
+    quantityElem.innerText = `${quantity > 0 ? --quantity : 0}`
 }
